921a64321e1d6d94ebcb686c749708bfd6076581
Jejis06 <wertex3233@gmail.com>
2023-02-09 23:43:24 +0100

    new features

--- src/TimeSort.h
+++ src/TimeSort.h
@@ -6,17 +6,32 @@
 /* Time sort */
 
 template<typename T>
-void TimeSort(T* arr, size_t size){
-    std::vector<std::future<int>> threads;
-    size_t it=0;
+T* Switch(T* beg, T* end){
+	T* ptr = beg;
+	T temp; 
+	for(T* it=beg; it<end; it++){
+		if(*it < 0) {
+			temp = *ptr;
+			*ptr = *it;
+			*it = temp;
 
+			ptr++;
+		}
+	}
+	return ptr-1;
+}
+template<typename T>
+void __Tsort(T* begin, T* end, short sign){
+    std::vector<std::future<int>> threads;
+    size_t size = (end - begin) * sign;
+    T* it = begin;
     for(int i=0; i<size; i++){
-	threads.push_back(std::async(std::launch::async,[&it,&arr,i](){
-		int time = static_cast<int>(arr[i]);
+	threads.push_back(std::async(std::launch::async,[&it,&begin,i,sign](){
+		int time = static_cast<int>(*(begin + i * sign));
 		std::this_thread::sleep_for(std::chrono::milliseconds(time)); 
-		arr[it] = time; 
-		it++;
-		
+		*it= time;
+		it+=sign;
+
 		return 1;
         }));
     }
@@ -24,4 +39,19 @@ void TimeSort(T* arr, size_t size){
         job.get();
 
 }
+
+template<typename T>
+void TimeSort(T* begin, T* end){
+	T* neg = Switch(begin,end);
+	printf("\n%i\n",*neg);
+
+	for(int i=0; i<(end-begin); i++)
+		printf("%i ",*(begin + i));
+
+	if(*neg< 0) __Tsort(neg,begin,-1);
+	__Tsort(neg,end,1);
+
+	return;
+}
 //there is no negative time 
+
--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,18 @@
 
 int main(){
 	/* load array and print it*/
-	double arr[] = {9,8,45,88,95,1,1,1,1,7,3,1};
-	for(int i=0; i<12; i++)
+	size_t size = 12;
+	double arr[size] = {9,8,45,88,95,1,1,1,1,7,3,1};
+	for(int i=0; i<size; i++)
 		std::cout << arr[i] << ' ';
 
 	/* sort array */
-	TimeSort(arr,12);
+	TimeSort(arr,arr+12);
+	std::cout << '\n';
+
 
 	/* print sorted array */
-	for(int i=0; i<12; i++)
+	for(int i=0; i<size; i++)
 		std::cout << arr[i] << ' ';
 
 	
